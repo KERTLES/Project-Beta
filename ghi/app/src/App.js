e9c4fc4e62780f568c01db5817eece60d988396d
Kurt <kurtgreen85@gmail.com>
2022-08-01 19:28:50 -0500

    added maufacturer list

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
+import ManufacturerList from './ManufacturerList';
 import Nav from './Nav';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturer" element={<ManufacturerList manufacturers={props.manufacturers} />} />
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,12 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="models">Models</NavLink>
+          </li>
+          <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="manufacturer">Manufacturer</NavLink>
+          </li>
           </ul>
         </div>
       </div>
