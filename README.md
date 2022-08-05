9406ba54bc7295463be09d9fc633321e446f1d55
Kurt <kurtgreen85@gmail.com>
2022-08-05 14:16:22 -0500

    fixing bugs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Team:
 
-* Person 1 - Which microservice?
+* Kurt Greenacre - Service
 * Kelly Jiang - Sales
 
 ## Design
--- ghi/app/src/ListSales.js
+++ ghi/app/src/ListSales.js
@@ -1,64 +1,59 @@
 import React from "react";
 
 function toString(num) {
-    let numbers = Number(num).toLocaleString()
-    return numbers
-
+  let numbers = Number(num).toLocaleString();
+  return numbers;
 }
 
 class ListSales extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            sales: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      sales: [],
+    };
+  }
+
+  async componentDidMount() {
+    const url = "http://localhost:8090/api/sales/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+
+      this.setState({ sales: data["Sales Record"] });
     }
+  }
 
-    async componentDidMount() {
-        const url = 'http://localhost:8090/api/sales/';
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
+  render() {
+    return (
+      <>
+        <h2>Sales History</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Sales Person</th>
+              <th>Employee number</th>
+              <th>Purchaser Name</th>
+              <th>VIN</th>
+              <th>Sale Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.sales.map((sale) => {
+              return (
+                <tr key={sale.vin.vin}>
+                  <td>{sale.sales_person.name}</td>
+                  <td>{sale.sales_person.employee_number}</td>
+                  <td>{sale.customer.name}</td>
+                  <td>{sale.vin.vin}</td>
 
-            this.setState({ sales: data["Sales Record"] })
-        }
-    }
-
-
-
-
-    render() {
-        return (
-            <>
-                <h2>Sales History</h2>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Sales Person</th>
-                            <th>Employee number</th>
-                            <th>Purchaser Name</th>
-                            <th>VIN</th>
-                            <th>Sale Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.sales.map(sale => {
-                            return (
-                                <tr key={sale.vin.vin}>
-                                    <td>{sale.sales_person.name}</td>
-                                    <td>{sale.sales_person.employee_number}</td>
-                                    <td>{sale.customer.name}</td>
-                                    <td>{sale.vin.vin}</td>
-
-                                    <td>{`$${toString(sale.price)}`}</td>
-
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </>
-        );
-    }
+                  <td>{`$${toString(sale.price)}`}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 export default ListSales;
